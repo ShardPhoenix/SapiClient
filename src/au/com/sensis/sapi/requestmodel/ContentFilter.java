90fd8d21f33dde873d21fab053f95f97cc9fd2a8
Ryan Sattler <ryan.sattler@sensis.com.au>
2012-03-23 16:12:10 +1100

    Updated licence

--- src/au/com/sensis/sapi/requestmodel/ContentFilter.java
+++ src/au/com/sensis/sapi/requestmodel/ContentFilter.java
@@ -4,7 +4,7 @@ public enum ContentFilter {
 	SHORT_DESCRIPTOR("shortDescriptor"), MEDIUM_DESCRIPTOR("mediumDescriptor"), DESCRIPTOR("descriptor"),
 	BUSINESS_LOGO("businessLogo"), IMAGE_GALLERY("imageGallery"), IMAGE("image"), VIDEO("video");
 	
-	private String name;
+	private final String name;
 
 	private ContentFilter(String name) {
 		this.name = name;
